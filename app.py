e692acdf0419d50bbe04751f6aba1913a96fcdfc
slabu <serj.labunets@gmail.com>
2020-10-28 01:10:50 +0200

    couple of solver methods

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, redirect, render_template
 
 from db import db
-from logic_algorithms.geometry import GeometrySolutions
+from logic_algorithms.geometry import GeometrySolutions, Rectangle, Circle, Parallelepiped
 
 
 app = Flask(__name__)
@@ -15,6 +15,35 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 def index():
     cube_params = GeometrySolutions.cube(cube_edge=5)
     print(cube_params)
+    
+    print("----------------------------------Rectangle-----------------------------------------")
+    rectangle_params = Rectangle(2, 3)
+    
+    print("Rectangle width: " + str(rectangle_params.rectangle_width))
+    print("Rectangle length: " + str(rectangle_params.rectangle_length))
+    print("Rectangle perimeter: " + str(rectangle_params.rectangle_perimeter))
+    print("Rectangle diagonal: " + str(rectangle_params.rectangle_diagonal))
+    print("Rectangle square: " + str(rectangle_params.rectangle_square))
+    
+    print("----------------------------------Circle--------------------------------------------")
+    circle_params = Circle(5)
+    print("Circle radius: " + str(circle_params.circle_radius))
+    print("Circle length: " + str(circle_params.circle_length))
+    print("Circle square: " + str(circle_params.circle_square))
+    
+    print("----------------------------------Parallelepiped------------------------------------")
+    parallelepiped_specs = Parallelepiped(5, 8, 10)
+    print("Parallelepiped length: " + str(parallelepiped_specs.parallelepiped_length))
+    print("Parallelepiped width: " + str(parallelepiped_specs.parallelepiped_width))
+    print("Parallelepiped height: " + str(parallelepiped_specs.parallelepiped_height))
+    print("Parallelepiped top side diagonal: " + str(parallelepiped_specs.parallelepiped_top_side_diagonal))
+    print("Parallelepiped front side diagonal: " + str(parallelepiped_specs.parallelepiped_front_side_diagonal))
+    print("Parallelepiped side diagonal: " + str(parallelepiped_specs.parallelepiped_side_diagonal))
+    print("Parallelepiped top side square: " + str(parallelepiped_specs.parallelepiped_top_side_square))
+    print("Parallelepiped front side square: " + str(parallelepiped_specs.parallelepiped_front_side_square))
+    print("Parallelepiped side square: " + str(parallelepiped_specs.parallelepiped_side_square))
+    print("Parallelepiped volume: " + str(parallelepiped_specs.parallelepiped_volume))
+
     return render_template('index.html', cube_params=cube_params)
 
 
--- logic_algorithms/geometry.py
+++ logic_algorithms/geometry.py
@@ -1,4 +1,4 @@
-from numpy import sqrt
+from numpy import sqrt, pi
 
 class GeometrySolutions():
     
@@ -29,4 +29,112 @@ class GeometrySolutions():
             cube_specs['cube_volume'] = cube_edge**3
             cube_specs['cube_side_perimeter'] = cls.square_perimeter(cube_edge)
             return cube_specs
+    
+
+class Rectangle():
+    
+    RectangleSpecs = {'rectangle_length': 0, 'rectangle_width': 0, 'rectangle_square': 0, 'rectanlge_diagonal': 0, 'rectangle_perimeter': 0}
+
+    def __init__(self, rectangle_length, rectangle_width):
+
+        self.rectangle_length = rectangle_length
+        
+        if rectangle_width is None:
+            self.rectangle_width = rectangle_length
+        else:
+            self.rectangle_width = rectangle_width
+        
+        #created specs
+
+        self.rectangle_square = Rectangle.rectangle_square(self.rectangle_length, self.rectangle_width)
+        self.rectangle_diagonal = Rectangle.rectangle_diagonal(self.rectangle_length, self.rectangle_width)
+        self.rectangle_perimeter = Rectangle.rectangle_perimeter(self.rectangle_length, self.rectangle_width)
+
+    @classmethod
+    def rectangle_square(cls, rectangle_length, rectangle_width):
+        return rectangle_length*rectangle_width
+
+    @classmethod
+    def rectangle_diagonal(cls, rectangle_length, rectangle_width):
+        return sqrt(rectangle_length**2 + rectangle_width**2)
+
+    @classmethod
+    def rectangle_perimeter(cls, rectangle_length, rectangle_width):
+        return (rectangle_width + rectangle_length)*2
+
+
+class Circle():
+    def __init__(self, circle_radius):
+        self.circle_radius = circle_radius
+        
+        #created specs
+
+        self.circle_square = Circle.circle_square(self.circle_radius)
+        self.circle_length = Circle.circle_length(self.circle_radius)
+
+
+    @classmethod
+    def circle_square(cls, circle_radius):
+        return pi*(circle_radius**2)
+
+    @classmethod
+    def circle_length(cls, circle_radius):
+        return 2*pi*circle_radius
+
+
+class Ellipse():
+    def __init__(self, ellipse_horizontal_half_axis, ellipse_vertical_half_axis):
+        self.horizontal_half_axis = horizontal_half_axis
+        self.vertical_half_axis = vertical_half_axis
+
+        #created specs
+
+
+class Parallelepiped():
+
+    parallelepiped_squares = {}
+    parallelepiped_diagonals = {}
+
+    def __init__(self, parallelepiped_length, parallelepiped_width, parallelepiped_height):
+        self.parallelepiped_length = parallelepiped_length
+        self.parallelepiped_width = parallelepiped_width
+        self.parallelepiped_height = parallelepiped_height
+
+        #created specs
+
+        parallelepiped_squares = Parallelepiped.parallelepiped_sides_squares(self.parallelepiped_length, self.parallelepiped_width, self.parallelepiped_height)
+        parallelepiped_diagonals = Parallelepiped.parallelepiped_sides_diagonals(self.parallelepiped_length, self.parallelepiped_width, self.parallelepiped_height)
+
+        self.parallelepiped_top_side_square = parallelepiped_squares['parallelepiped_top_side_square']
+        self.parallelepiped_front_side_square = parallelepiped_squares['parallelepiped_front_side_square']
+        self.parallelepiped_side_square = parallelepiped_squares['parallelepiped_side_square']
+        self.parallelepiped_top_side_diagonal = parallelepiped_diagonals['parallelepiped_top_side_diagonal']
+        self.parallelepiped_front_side_diagonal = parallelepiped_diagonals['parallelepiped_front_side_diagonal']
+        self.parallelepiped_side_diagonal = parallelepiped_diagonals['parallelepiped_side_diagonal']
+        self.parallelepiped_volume = Parallelepiped.parallelepiped_volume(self.parallelepiped_length, self.parallelepiped_width, self.parallelepiped_height)
+
+    @classmethod
+    def parallelepiped_sides_squares(cls, parallelepiped_length, parallelepiped_width, parallelepiped_height):
+        parallelepiped_top_side_square = Rectangle.rectangle_square(parallelepiped_length, parallelepiped_width)
+        parallelepiped_front_side_square = Rectangle.rectangle_square(parallelepiped_length, parallelepiped_height)
+        parallelepiped_side_square = Rectangle.rectangle_square(parallelepiped_width, parallelepiped_height)
+
+        return {'parallelepiped_top_side_square': str(parallelepiped_top_side_square), 
+                'parallelepiped_front_side_square': str(parallelepiped_front_side_square), 
+                'parallelepiped_side_square': str(parallelepiped_side_square)}
+    
+    @classmethod
+    def parallelepiped_sides_diagonals(cls, parallelepiped_length, parallelepiped_width, parallelepiped_height):
+        parallelepiped_top_side_diagonal = Rectangle.rectangle_diagonal(parallelepiped_length, parallelepiped_width)
+        parallelepiped_front_side_diagonal = Rectangle.rectangle_diagonal(parallelepiped_length, parallelepiped_height)
+        parallelepiped_side_diagonal = Rectangle.rectangle_diagonal(parallelepiped_width, parallelepiped_height)
+
+        return {'parallelepiped_top_side_diagonal': parallelepiped_top_side_diagonal,
+                'parallelepiped_front_side_diagonal': parallelepiped_front_side_diagonal,
+                'parallelepiped_side_diagonal': parallelepiped_side_diagonal}
+
+    @classmethod
+    def parallelepiped_volume(cls, parallelepiped_length, parallelepiped_width, parallelepiped_height):
+        return parallelepiped_length*parallelepiped_width*parallelepiped_height
+
     
