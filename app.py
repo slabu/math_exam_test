14d48bf228bd3d7c12e67a1d6704c1bd367fda67
slabu <serj.labunets@gmail.com>
2020-12-26 23:41:41 +0200

    Adding uwsgi to requirements

--- app.py
+++ app.py
@@ -24,9 +24,6 @@ manager = LoginManager(app)
 def load_user(user_id):
     return UserModel.query.get(user_id)
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
     
     #admin_user = UserModel(user_login='admin', user_password='admin', user_access_level='admin')
     #admin_user.save_to_db()
